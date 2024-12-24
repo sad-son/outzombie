a57001056b3647ddcadb140d23c34b225ef8b528
Ayaz Sadykov <ayaz.sadykov@mytona.com>
2024-12-24 22:28:15 +0500

    added entry scene editor button

---
+++ outzombie/Assets/Editor/RecompileToolbar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace M3.UnityToolbarExtender
+{
+    [InitializeOnLoad]
+    public static class RecompileToolbar
+    {
+        static RecompileToolbar()
+        {
+            ToolbarExtender.RightToolbarGUI.Remove(OnToolbarGUI);
+            ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+        }
+
+        private static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+
+            if (EditorSettings.enterPlayModeOptions.HasFlag(EnterPlayModeOptions.DisableDomainReload))
+            {
+                EditorGUILayout.LabelField(EditorGUIUtility.TrTextContentWithIcon(
+                        "Domain Reload is disabled", "Static changes may still be present", MessageType.Info),
+                    EditorStyles.miniBoldLabel);
+            }
+
+            if (GUILayout.Button(new GUIContent("Recompile", "Request Script Compilation"),
+                    EditorStyles.toolbarButton))
+            {
+                CompilationPipeline.RequestScriptCompilation();
+            }
+        }
+    }
+}
---
+++ outzombie/Assets/Editor/StartSceneToolbar.cs
@@ -0,0 +1,50 @@
+﻿using M3.UnityToolbarExtender;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public static class StartSceneToolbar
+    {
+        private static SceneAsset startScene
+        {
+            get
+            {
+                var path = "Assets/Scenes/Entry.unity";
+                return AssetDatabase.LoadAssetAtPath<SceneAsset>(path);
+            }
+        }
+
+        static StartSceneToolbar()
+        {
+            ToolbarExtender.LeftToolbarGUI.Remove(OnToolbarGUI);
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+
+            EditorApplication.playModeStateChanged += ResetStartScene;
+        }
+
+        private static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button(new GUIContent($"{startScene.name}", $"Start {startScene.name} Scene"),
+                    EditorStyles.toolbarButton))
+            {
+                EditorSceneManager.playModeStartScene = startScene;
+                EditorApplication.isPlaying = true;
+            }
+
+            return;
+        }
+
+        private static void ResetStartScene(PlayModeStateChange state)
+        {
+            if (EditorSceneManager.playModeStartScene != startScene ||
+                state != PlayModeStateChange.ExitingPlayMode) return;
+
+            EditorSceneManager.playModeStartScene = null;
+        }
+    }
+}
